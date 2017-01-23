67d5d97d6c96b1ccf8622f4462684f8bc92ba513
Ryan Hallisey <rhallise@redhat.com>
2017-01-23 16:11:03 -0500

    Fix mispelling in config file

--- containers/mariadb/start.sh
+++ containers/mariadb/start.sh
@@ -4,7 +4,7 @@ function boostrap {
     mysqld_safe --wsrep-new-cluster &
 
     mysql -u root --password="password" -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION;"
-    mysql -u root --password="password" -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'passworod' WITH GRANT OPTION;"
+    mysql -u root --password="password" -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'password' WITH GRANT OPTION;"
     mysqladmin -uroot -p "password" shutdown
 }
 
