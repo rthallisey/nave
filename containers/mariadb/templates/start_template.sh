e0e5857e18be0b92a1b66d1fc13f0efc89d8ed13
Ryan Hallisey <rhallise@redhat.com>
2017-01-24 18:14:43 -0500

    Add an expect script to set db password

--- containers/mariadb/templates/start_template.sh
+++ containers/mariadb/templates/start_template.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
-function boostrap {
+set -x
+set -e
+
+function bootstrap {
     mysqld_safe --wsrep-new-cluster &
 
-    mysql -u root --password="password" -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION;"
-    mysql -u root --password="password" -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'password' WITH GRANT OPTION;"
-    mysqladmin -uroot -p "password" shutdown
+    while [[ ! -f /var/lib/mysql/mariadb.pid ]]; do
+        sleep 3
+    done
+
+    ./security_reset.exp
+    mysql -u root --password="{{db_password}}" -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '{{db_password}}' WITH GRANT OPTION;"
+    mysql -u root --password="{{db_password}}" -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '{{db_password}}' WITH GRANT OPTION;"
+    mysqladmin -uroot -p "{{db_password}}" shutdown
 }
+
+mkdir -p /var/lib/mysql
+chown -R mysql: /var/lib/mysql
+
+tail -f /var/log/mariadb/mariadb.log &
 
 if [[ "${!BOOTSTRAP[@]}" ]]; then
     mysql_install_db
+    chown -R mysql: /var/lib/mysql
     bootstrap
     exit 0
 fi
 
-tail -f /var/log/mariadb/mariadb.log &
 /usr/bin/mysqld_safe
